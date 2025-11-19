1f3f0efceaed6181f5826d173ace9ef70947462c
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-11-18 20:40:32 -0500

    Add getters to breed and move resources

--- project/monsters/breed/breed.gd
+++ project/monsters/breed/breed.gd
@@ -6,3 +6,19 @@ extends Resource
 @export var _moves: Array[Move]
 @export var _health_points: int
 @export var _speed: int
+
+
+func get_monster_name() -> String:
+	return _monster_name
+
+func get_type() -> Types.Type:
+	return _type
+
+func get_moves() -> Array[Move]:
+	return _moves
+
+func get_health_points() -> int:
+	return _health_points
+
+func get_speed() -> int:
+	return _speed
--- project/moves/move.gd
+++ project/moves/move.gd
@@ -5,4 +5,24 @@ extends Resource
 @export var _type: Types.Type
 @export var _attack_power: int
 @export var _attempts: int
-@export var _accuract: int
+@export var _accuracy: int
+
+
+func get_move_name() -> String:
+	return _move_name
+
+
+func get_type() -> Types.Type:
+	return _type
+
+
+func get_attack_power() -> int:
+	return _attack_power
+
+
+func get_attempts() -> int:
+	return _attempts
+
+
+func get_accuracy() -> int:
+	return _accuracy
